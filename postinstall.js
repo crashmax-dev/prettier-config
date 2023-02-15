5dcff731bc673d4a17fac40d4ebca984cec1f1ab
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-02-15 18:21:59 +0800

    add prettier config path

--- postinstall.js
+++ postinstall.js
@@ -5,6 +5,12 @@ import { join } from 'node:path'
 
 try {
   const workDir = process.cwd()
+  const prettierConfigPath = join(
+    workDir,
+    'node_modules',
+    '@crashmax',
+    'prettier-config'
+  )
   const packageJsonPath = join(workDir, 'package.json')
   const packageJson = await readFile(packageJsonPath, 'utf-8')
   const parsedPackageJson = JSON.parse(packageJson)
@@ -15,8 +21,8 @@ try {
     JSON.stringify(parsedPackageJson, null, 2) + '\n',
     'utf-8'
   )
-  await copyFile('.pretterignore', workDir)
-  await copyFile('.prettierrc.cjs', workDir)
+  await copyFile(join(prettierConfigPath, '.pretterignore'), workDir)
+  await copyFile(join(prettierConfigPath, '.prettierrc.cjs'), workDir)
 
   console.log('@crashmax/prettier-config installed successfully!')
 } catch (err) {
